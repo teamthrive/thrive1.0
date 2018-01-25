7aaff4f2cef722e9b66922a850c2450aec4f6eed
xianmingli <xmli@stanford.edu>
2018-01-24 21:58:33 -0800

    Finished first draft

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,46 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Thrive';
+  selectedSection: number = 1;
+
+  myThrivePreferences = {
+    "incorpReasons": {
+      "limitLiab": false,
+      "simpTax": false,
+      "professional": false,
+      "business": false,
+      "fundraise":false
+    },
+    "typeCorp": "llc",
+    "corpInfo": {
+      "name": "Thrive",
+      "regAgent": "Stephen Blank",
+      "purpose": "1099 tax solution",
+      "organizer": "Lean Launchpad",
+      "members": "Trent, Wendy, Sean, Parker, Sahil",
+      "manageStruct": "Awesome team of 5",
+      "address1": "123 Thrive Avenue",
+      "address2": "",
+      "city": "Stanford",
+      "state": "CA",
+      "zip": "94305"
+    }
+  }
+  
+  applyIncorp() {
+    console.log(this.myThrivePreferences.corpInfo)
+    this.selectedSection = this.selectedSection + 1;
+  }
+
+  nextClicked() {
+    console.log(this.myThrivePreferences)
+    this.selectedSection = this.selectedSection + 1;
+  }
+
+  prevClicked() {
+    console.log(this.myThrivePreferences)
+    if(this.selectedSection > 1) {
+      this.selectedSection = this.selectedSection - 1;
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
@@ -12,7 +12,8 @@ import { HeaderComponent } from './components/header/header.component';
     HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
